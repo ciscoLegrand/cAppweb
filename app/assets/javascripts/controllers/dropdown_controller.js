85521b373b5d14a2d47a4822118b828da7216717
ciscoLegrand <cisco.glez@gmail.com>
2022-01-18 21:45:55 +0100

    [add] stimulus dropdown controller

--- app/assets/javascripts/controllers/dropdown_controller.js
+++ app/assets/javascripts/controllers/dropdown_controller.js
@@ -2,9 +2,16 @@ import { Controller } from "stimulus"
 
 export default class extends Controller {
 
-	static targets = [ "button" ]
+	static targets = [ "button", "productfeatures" ]
 
 	show_options() {
 	 	this.buttonTarget.classList.toggle('is-block');	 	
+	}
+
+	display_product_features(e) {
+		e.preventDefault()
+		this.productfeaturesTarget.classList.contains("is-hidden") ?
+		this.productfeaturesTarget.classList.remove('is-hidden') : 
+		this.productfeaturesTarget.classList.add('is-hidden')
 	}
 }
