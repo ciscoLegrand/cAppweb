77ce3b87423ec53aa411c164427e2e74cf8707e1
ciscoLegrand <cisco.glez@gmail.com>
2022-01-18 21:19:06 +0100

    [style] add web service product template from cadmin

--- app/assets/javascripts/controllers/flatpickr_controller.js
+++ app/assets/javascripts/controllers/flatpickr_controller.js
@@ -3,10 +3,10 @@ import flatpickr from 'https://cdn.skypack.dev/stimulus-flatpickr';
 import { Spanish } from 'https://esm.sh/flatpickr/dist/l10n/es.js';
 export default class extends flatpickr {
   initialize() {
-    // sets your language (you can also set some global setting for all time pickers)
-    this.config = {
-      locale: Spanish
-    }
+    // // sets your language (you can also set some global setting for all time pickers)
+    // this.config = {
+    //   locale: Spanish
+    // }
   }
 }
 
