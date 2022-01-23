16a662b339aeffe8d3e7d7ada74cbcd7f0ea528f
ciscoLegrand <cisco.glez@gmail.com>
2022-01-23 14:18:14 +0100

    [style] navbar and down to 50 char validation body contact mail

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,5 +3,5 @@ class Contact < ApplicationRecord
 
   validates :email, :title, :body, presence: true
   validates :title, length: {minimum: 10 , too_short: "Minimo de caracteres %{count} caracteres."}
-  validates :body, length: {minimum: 100, too_short: "Minimo de caracteres %{count} caracteres."}
+  validates :body, length: {minimum: 50, too_short: "Minimo de caracteres %{count} caracteres."}
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = { 
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'gmail.com',
+    domain:               ENV['SMTP_DOMAIN'],
     user_name:            ENV['GMAIL_ACCOUNT'],
     password:             ENV['GMAIL_PASS'],
     authentication:       'plain'
