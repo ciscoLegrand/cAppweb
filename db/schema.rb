2e35e4bade07e2929f620af9f4592db7515a8051
ciscoLegrand <cisco.glez@gmail.com>
2022-01-27 22:32:55 +0100

    [upgrade] cadmin latest development commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_03_182420) do
+ActiveRecord::Schema.define(version: 2022_01_27_203901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -352,7 +352,7 @@ ActiveRecord::Schema.define(version: 2022_01_03_182420) do
     t.string "role", default: "user", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "slug"
-    t.integer "customer_id"
+    t.string "stripe_customer_id"
     t.integer "subscription_id"
     t.integer "price_id"
     t.string "reset_password_token"
