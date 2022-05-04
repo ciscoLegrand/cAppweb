9e08998e39353a0526caf382da9cc8a10e36f72a
ciscoLegrand <cisco.glez@gmail.com>
2022-05-04 19:36:49 +0200

    [fixtmp] send email on development env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -86,19 +86,23 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   
+  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # config.action_mailer.delivery_method = :letter_opener
+  # config.action_mailer.perform_deliveries = true
+
+
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.delivery_method = :letter_opener
-  config.action_mailer.perform_deliveries = true
-  # config.action_mailer.delivery_method = :smtp 
-  # config.action_mailer.smtp_settings = { 
-  #   address:              'smtp.gmail.com',
-  #   port:                 587,
-  #   domain:               ENV['SMTP_DOMAIN'],
-  #   user_name:            ENV['GMAIL_ACCOUNT'],
-  #   password:             ENV['GMAIL_PASS'],
-  #   authentication:       'plain'
-  #   # enable_starttls_auto: true
+  config.action_mailer.default_url_options = { host: ENV['SMTP_DOMAIN'], port: 3000 }
+  config.action_mailer.delivery_method = :smtp 
+  config.action_mailer.smtp_settings = { 
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               ENV['SMTP_DOMAIN'],
+    user_name:            ENV['GMAIL_ACCOUNT'],
+    password:             ENV['GMAIL_PASS'],
+    authentication:       'plain'
+    # enable_starttls_auto: true
     # ^ ^ remove this option ^ ^
-  #  }
+   }
 end
